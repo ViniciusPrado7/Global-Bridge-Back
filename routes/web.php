408e51b0814c192b0f95b6c8a23741d3fcc98641
ViniciusPrado7 <vinicius.pradofake@gmail.com>
2026-02-20 14:57:14 -0300

    feat: json status ok to inical page /

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'status' => 'ok',
+        'service' => 'Global Bridge API',
+        'environment' => app()->environment()
+    ]);
 });
