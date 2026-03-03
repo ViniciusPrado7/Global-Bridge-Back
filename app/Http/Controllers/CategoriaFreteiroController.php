b807a648a029a77c56910560aac1b8dbb16c76f0
Lucas Sant <lucasfdesantana095@gmail.com>
2026-03-03 16:59:47 -0300

    feat: create categoria migration

---
+++ app/Http/Controllers/CategoriaFreteiroController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Categoria_Freteiro;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CategoriaFreteiroController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Categoria_Freteiro $categoria_Freteiro)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Categoria_Freteiro $categoria_Freteiro)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Categoria_Freteiro $categoria_Freteiro)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Categoria_Freteiro $categoria_Freteiro)
+    {
+        //
+    }
+}
---
+++ app/Models/Categoria_Freteiro.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Categoria_Freteiro extends Model
+{
+    protected $fillabel = [
+        'id_categoria',
+        'id_freteiro'
+    ];
+}
