b985d97cc595743dc42f0ac90ab46fe85c28ebb5
Matheus Batista <aluiziomatheus@gmail.com>
2026-03-04 08:45:26 -0300

    feat: add all changes

--- routes/api.php
+++ routes/api.php
@@ -3,14 +3,23 @@
 use App\Http\Controllers\CargaController;
 use App\Http\Controllers\CargaItemController;
 use App\Http\Controllers\FreteiroController;
-use App\Http\Controllers\UserNameController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\EmbarqueController;
+use App\Http\Controllers\InvoiceController;
+use App\Http\Controllers\PagamentoController;
 
+Route::apiResource('cargas/{carga}/pagamentos', PagamentoController::class);
 Route::apiResource('cargas', CargaController::class);
 Route::apiResource('itens', CargaItemController::class)
     ->parameters([
         'itens' => 'cargaItem'
     ]);
 Route::apiResource('freteiros', FreteiroController::class);
-Route::apiResource('usernames',UserNameController::class)
- ->parameters(['usernames' => 'userName']);
+Route::apiResource('tasks', TaskController::class);
+Route::apiResource('clientes', ClienteController::class);
+Route::apiResource('embarques', EmbarqueController::class);
+Route::apiResource('invoices', InvoiceController::class);
+
+
